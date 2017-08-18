041db67ae3fefe8a86673aa07225b85088f101a7
matheus morcinek <matheusmorcinek@gmail.com>
2017-08-18 18:36:14 -0300

    Added - marsrover commands file txt and app.java updated.

--- App.java
+++ App.java
@@ -1,5 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class App{
 	public static void main (String args[]){
-			System.out.println("Hello Rover!");
+	
+		Scanner ler = new Scanner(System.in);
+		 
+	    System.out.printf("Informe o nome de arquivo texto:\n");
+	    String nome = ler.nextLine();
+	 
+	    System.out.printf("\nConteúdo do arquivo texto:\n");
+	    
+	    try {
+	      FileReader arq = new FileReader(nome);
+	      BufferedReader lerArq = new BufferedReader(arq);
+	 
+	      String linha = lerArq.readLine(); 
+	      while (linha != null) {
+	       
+	    	  
+	    	  System.out.printf("%s\n", linha);
+	    	  String comando = ler.nextLine();
+	    	  
+	    	  
+	    	  
+	    	  linha = lerArq.readLine(); 
+	      }
+	 
+	      arq.close();
+	    } catch (IOException e) {
+	        System.err.printf("Erro na abertura do arquivo: %s.\n",
+	          e.getMessage());
+	    }
+	 
+	    System.out.println();
+	  
 	}
 }
