75ba24ea2619b7542aa7eb3299b5ec3f5d689683
16280012 <16280012@l1747344.portoalegre.pucrsnet.br>
2017-08-18 18:33:20 -0300

    add getX method

--- MarsRover.java
+++ MarsRover.java
@@ -1,3 +1,16 @@
 public class MarsRover{
+private int x;
+private int y;
+private int pos;
+
+public MarsRover(int x, int y, int pos){
+this.x = x;
+this.y = y;
+this.pos = pos;
+}
+
+public int getX(int x){
+return x;
+}
 
 }
